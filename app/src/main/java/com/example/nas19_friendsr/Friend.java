02e0d286f4fe9335c6d6076e3f5ff6919d4d5292
david-knigge <davidknigge@live.nl>
2019-02-18 14:55:51 +0100

    adding friend

--- app/src/main/java/com/example/nas19_friendsr/Friend.java
+++ app/src/main/java/com/example/nas19_friendsr/Friend.java
@@ -1,8 +1,11 @@
 package com.example.nas19_friendsr;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.Serializable;
 
-public class Friend implements Serializable {
+public class Friend implements Serializable, Parcelable {
     private String name, bio;
     private int drawableId;
     private float rating;
@@ -12,6 +15,25 @@ public class Friend implements Serializable {
         this.bio = bio;
         this.drawableId = drawableId;
     }
+
+    protected Friend(Parcel in) {
+        name = in.readString();
+        bio = in.readString();
+        drawableId = in.readInt();
+        rating = in.readFloat();
+    }
+
+    public static final Creator<Friend> CREATOR = new Creator<Friend>() {
+        @Override
+        public Friend createFromParcel(Parcel in) {
+            return new Friend(in);
+        }
+
+        @Override
+        public Friend[] newArray(int size) {
+            return new Friend[size];
+        }
+    };
 
     public void setRating(float rating) {
         this.rating = rating;
@@ -25,11 +47,29 @@ public class Friend implements Serializable {
         return bio;
     }
 
+    public void setName(String name) { this.name = name; }
+
+    public void setBio(String bio) { this.bio = bio; }
+
     public int getDrawableId() {
         return drawableId;
     }
 
     public float getRating() {
         return rating;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeString(name);
+        dest.writeString(bio);
+        dest.writeInt(drawableId);
+        dest.writeFloat(rating);
     }
 }
--- app/src/main/java/com/example/nas19_friendsr/MainActivity.java
+++ app/src/main/java/com/example/nas19_friendsr/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.nas19_friendsr;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,26 +24,68 @@ public class MainActivity extends AppCompatActivity {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             Friend clickedFriend = (Friend) parent.getItemAtPosition(position);
             Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-            intent.putExtra("clicked_friend", clickedFriend);
+            intent.putExtra("clicked_friend", (Serializable) clickedFriend);
             startActivity(intent);
         }
     }
 
-    String[] persons = new String[] { "arya", "cersei", "daenerys", "jaime",
-        "jon", "jorah", "margaery", "melisandre", "sansa", "tyrion"
-    };
+    ArrayList<Friend> friends;
+    static int newFriends = 0;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addItem:
+                newFriends += 1;
+                friends.add(new Friend("New Friend " + newFriends, "Edit this bio...", getResources().getIdentifier("tyrion" , "drawable", getPackageName())));
+                setAdapter();
+                break;
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Friend> friends = new ArrayList<>();
-
-        for (String person:persons) {
-            friends.add(new Friend(person, "standard", getResources().getIdentifier(person , "drawable", getPackageName())));
+        if (savedInstanceState != null) {
+            friends = savedInstanceState.getParcelableArrayList("friends");
+        } else {
+            friends = new ArrayList<>();
+            setFriends();
         }
 
+        setAdapter();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("friends", friends);
+    }
+
+    public void setFriends() {
+        friends.add(new Friend("Arya", "standard", getResources().getIdentifier("arya" , "drawable", getPackageName())));
+        friends.add(new Friend("Cersei", "standard", getResources().getIdentifier("cersei" , "drawable", getPackageName())));
+        friends.add(new Friend("Daenerys", "standard", getResources().getIdentifier("daenerys" , "drawable", getPackageName())));
+        friends.add(new Friend("Jaime", "standard", getResources().getIdentifier("jaime" , "drawable", getPackageName())));
+        friends.add(new Friend("Jon", "standard", getResources().getIdentifier("jon" , "drawable", getPackageName())));
+        friends.add(new Friend("Jorah", "standard", getResources().getIdentifier("jorah" , "drawable", getPackageName())));
+        friends.add(new Friend("Margaery", "standard", getResources().getIdentifier("margaery" , "drawable", getPackageName())));
+        friends.add(new Friend("Melisandre", "standard", getResources().getIdentifier("melisandre" , "drawable", getPackageName())));
+        friends.add(new Friend("Sannsa", "standard", getResources().getIdentifier("sansa" , "drawable", getPackageName())));
+        friends.add(new Friend("Tyrion", "standard", getResources().getIdentifier("tyrion" , "drawable", getPackageName())));
+    }
+
+    public void setAdapter() {
         FriendsAdapter adapter = new FriendsAdapter(this, R.layout.grid_item, friends);
         GridView view = findViewById(R.id.mainContainer);
         view.setAdapter(adapter);
